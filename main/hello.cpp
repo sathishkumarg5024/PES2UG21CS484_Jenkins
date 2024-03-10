bf310da4e067f3cb635ff7becfc8a9950faf732f
Sathish Kumar G <sathishkumarg5024@gmail.com>
2024-03-10 16:45:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, World satish\n";
+  cout << "Hello, Jenkins satisha\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
